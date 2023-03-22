060dee1cfb497528fef4495f048f1856e0ba57f0
Renukumar143 <128582106+Renukumar143@users.noreply.github.com>
2023-03-22 20:29:19 +0530

    suma

---
+++ javaprogram1.java
@@ -0,0 +1,17 @@
+package file;
+
+public class javaprogram1 {
+//How do we reverse a string
+	public static void main(String[] args) {
+		String a = "java program";
+		String b = "";
+		char c;
+		for (int i = a.length() - 1; i >= 0; i--) {
+			c = a.charAt(i);
+			b += c;
+		}
+		System.out.println("original String :" + a);
+		System.out.println("Reverse :" + b);
+	}
+
+}
